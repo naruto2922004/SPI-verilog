b045efbaafc77a6c0d82a78b4756b240652d01d2
naruto2922004 <123247872+naruto2922004@users.noreply.github.com>
2026-02-18 23:56:11 +0530

    Update and rename SPI_test.v to SPI1_test.v

--- SPI1_test.v
+++ SPI1_test.v
@@ -1,4 +1,4 @@
-module SPI_test;
+module SPI1_test;
 reg transmit, CLK_M, CLK_S, reset, d_valid;
 wire MISO;
 reg [7:0] data_M;
@@ -9,7 +9,7 @@ MASTER ins1(transmit, MISO, CLK_M, reset, d_valid, data_M, MOSI, SCLK, CS, done_
 reg [7:0]data_S;
 wire done_S;
 wire [7:0]rx_S;
-SLAVE ins2(MOSI, SCLK, CS, reset, CLK_S, data_S, MISO, done_S, rx_S);
+SLAVE1 ins2(MOSI, SCLK, CS, reset, CLK_S, data_S, MISO, done_S, rx_S);
 
 always #5 CLK_M = ~CLK_M;
 always #2 CLK_S = ~CLK_S;
@@ -33,4 +33,5 @@ data_S = 8'b0;
 #10;
 end
 endmodule
+
 
